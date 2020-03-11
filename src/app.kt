7310f84003b71e7cd27facddb3ffd13c288455bf
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-03-11 10:51:09 +0100

    Initial commit

---
+++ src/app.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+val abelardo = Persona("Alelardo","Bretaña",10)
+println(abelardo.toString())
+println(Persona.toString())
+val luis = Persona("Luish","Bretaña",10)
+println(luis.toString())
+println(Persona.toString())
+val noe = Persona("Neo","Dos Bancos",12)
+println(noe.toString())
+println(Persona.toString())
+val samu = Persona("Samuel","Verde",5)
+println(samu.toString())
+println(Persona.toString())
+val marcos = Persona("Marcos","Alvarez",3)
+println(marcos.toString())
+println(Persona.toString())
+println(PersonaUnica.toString())
+}
